9d9c380d9d80507431d364555bb3a131f6aaac72
Jarrod Kallis <jarrod.kallis@altron.com>
2017-12-04 09:16:40 +0200

    Conditional list

--- src/App.js
+++ src/App.js
@@ -8,19 +8,23 @@ class App extends Component {
   state = {
     persons: [
       {
+        id: 1,
         name: 'Max',
         age: 28
       },
       {
+        id: 2,
         name: 'Manu',
         age: 29,
         extra: 'My Hobbies: Racing'
       },
       {
+        id: 3,
         name: 'Stephanie',
         age: 26
       }
-    ]
+    ],
+    showPersons: false
   };
 
   switchNameHandler = () => {
@@ -30,16 +34,84 @@ class App extends Component {
     this.setState({ persons });
   };
 
+  showPersonsHandler = () => {
+    this.setState({ showPersons: !this.state.showPersons });
+  };
+
+  deletePersonHandler = index => {
+    // const persons = this.state.persons.slice();
+    // const persons = [...this.state.persons];
+    // persons.splice(index, 1);
+    // this.setState({ persons });
+
+    this.setState({
+      persons: [
+        ...this.state.persons.slice(0, index),
+        ...this.state.persons.slice(index + 1)
+      ]
+    });
+  };
+
+  nameChangeHandler = (event, id) => {
+    // 'findIndex' will iterate over each element in the array and
+    // return the index if the compare statement returns true
+    const personIndex = this.state.persons.findIndex(
+      tmpPerson => tmpPerson.id === id
+    );
+
+    const person = {
+      ...this.state.persons[personIndex],
+      name: event.target.value
+    };
+
+    // Update state this way:
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons });
+
+    // Or this way:
+    // this.setState({
+    //   persons: [
+    //     ...this.state.persons.slice(0, personIndex),
+    //     person,
+    //     ...this.state.persons.slice(personIndex + 1)
+    //   ]
+    // });
+  };
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer',
+      marginRight: '5px'
+    };
+
     return (
       <div className="App">
         <h1>Hi from React</h1>
-        <button onClick={() => this.switchNameHandler()}>Switch Name</button>
-        {this.state.persons.map(person => (
-          <Person key={person.name} name={person.name} age={person.age}>
-            {person.extra}
-          </Person>
-        ))}
+        <button style={style} onClick={() => this.switchNameHandler()}>
+          Switch Name
+        </button>
+        <button style={style} onClick={() => this.showPersonsHandler()}>
+          {this.state.showPersons ? 'Hide Persons' : 'Show Persons'}
+        </button>
+        {this.state.showPersons
+          ? this.state.persons.map((person, index) => (
+              <Person
+                key={person.id}
+                name={person.name}
+                age={person.age}
+                deleteClick={() => this.deletePersonHandler(index)}
+                change={event => this.nameChangeHandler(event, person.id)}
+              >
+                {person.extra}
+              </Person>
+            ))
+          : null}
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import './Person.css';
+
 const Person = props => (
-  <div>
-    I am {props.name} and I am {props.age}!
+  <div className="Person">
+    <p>
+      I am {props.name} and I am {props.age}!
+    </p>
+    <input onChange={props.change} value={props.name} />
     <p>{props.children}</p>
+    <button onClick={props.deleteClick}>Delete Person</button>
   </div>
 );
 
@@ -15,7 +21,9 @@ Person.defaultProps = {
 Person.propTypes = {
   name: PropTypes.string.isRequired,
   age: PropTypes.number.isRequired,
-  children: PropTypes.string
+  children: PropTypes.string,
+  deleteClick: PropTypes.func.isRequired,
+  change: PropTypes.func.isRequired
 };
 
 export default Person;
