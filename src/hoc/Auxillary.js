204a47d4131feffadf6adfd1119063894fb371c6
Jarrod Kallis <jarrod.kallis@gmail.com>
2017-12-06 08:36:02 +0200

    Auxillary (Fragment) HOC as implemented in React 16.2

--- src/hoc/Auxillary.js
+++ src/hoc/Auxillary.js
@@ -1,3 +1,11 @@
+// React 16.2 implements this 'fragment' HOC for you.
+// Usage:
+// render() {
+//   <>
+//     <h1>First Element</h1>
+//     <h1>Second Element</h1>
+//   </>
+// }
 const Auxillary = props => props.children;
 
 export default Auxillary;
